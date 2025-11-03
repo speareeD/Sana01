01483a473a798c376ad2992a47b1979b9829ae71
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-11-03 12:33:14 +0200

    Fixed names for variables and added another needed one

--- LinearExpressions2/Program.cs
+++ LinearExpressions2/Program.cs
@@ -1,9 +1,10 @@
-﻿double a, b, c, d;
+﻿double a, b, x, m, n;
 double z1, z2, y;
 
 Console.WriteLine("Enter a, b, c, d (by enter): ");
 a = Convert.ToInt32(Console.ReadLine());
 b = Convert.ToInt32(Console.ReadLine());
-c = Convert.ToInt32(Console.ReadLine());
-d = Convert.ToInt32(Console.ReadLine());
+x = Convert.ToInt32(Console.ReadLine());
+m = Convert.ToInt32(Console.ReadLine());
+n = Convert.ToInt32(Console.ReadLine());
 
