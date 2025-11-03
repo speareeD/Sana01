abfbeb5fda4fdac46539267b050ac87933f0beb1
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-11-03 12:31:20 +0200

    User in now able to enter a, b, c, d

--- LinearExpressions2/Program.cs
+++ LinearExpressions2/Program.cs
@@ -1,3 +1,9 @@
 ﻿double a, b, c, d;
 double z1, z2, y;
 
+Console.WriteLine("Enter a, b, c, d (by enter): ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+d = Convert.ToInt32(Console.ReadLine());
+
