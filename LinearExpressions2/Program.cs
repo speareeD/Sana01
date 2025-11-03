2750e90dc45aae73a9c8c03cbe4a861a617bcc7e
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-11-03 12:45:19 +0200

    Changed data types and methods for converting input to double

--- LinearExpressions2/Program.cs
+++ LinearExpressions2/Program.cs
@@ -2,12 +2,16 @@
 double z1, z2, y;
 
 Console.WriteLine("Enter a, b, c, d (by enter): ");
-a = Convert.ToInt32(Console.ReadLine());
-b = Convert.ToInt32(Console.ReadLine());
-x = Convert.ToInt32(Console.ReadLine());
-m = Convert.ToInt32(Console.ReadLine());
-n = Convert.ToInt32(Console.ReadLine());
+a = double.Parse(Console.ReadLine());
+b = double.Parse(Console.ReadLine());
+x = double.Parse(Console.ReadLine());
+m = double.Parse(Console.ReadLine());
+n = double.Parse(Console.ReadLine());
 
 z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(m*m*m*n) + n*m + m*m - m);
 z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
 y = 2.4 * Math.Abs((x * x + b) / a) + (a-b)*Math.Pow(Math.Sin(a-b), 2) + Math.Pow(10, -2) * (x-b);
+
+Console.WriteLine($"z1 = {z1}");
+Console.WriteLine($"z2 = {z2}");
+Console.WriteLine($"y = {y}");
