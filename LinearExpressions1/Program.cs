2c1f076a918a5409174d64146a869a00a39af2aa
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-11-03 12:05:13 +0200

    a, b, c, d is for user input

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -1,3 +1,12 @@
 ﻿double a, b, c, d;
-
 double x, y, z, r;
+
+Console.WriteLine("Enter a, b, c, d (by enter): ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+d = Convert.ToInt32(Console.ReadLine());
+
+x = (a + 2 * b - c + d) / (c * d);
+
+Console.WriteLine(x);
