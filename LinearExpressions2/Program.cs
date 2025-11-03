82a45e3d9f84f2bfc164a3fbdf331446dfc976fd
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2025-11-03 12:39:51 +0200

    Added math examples

--- LinearExpressions2/Program.cs
+++ LinearExpressions2/Program.cs
@@ -8,3 +8,6 @@ x = Convert.ToInt32(Console.ReadLine());
 m = Convert.ToInt32(Console.ReadLine());
 n = Convert.ToInt32(Console.ReadLine());
 
+z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(m*m*m*n) + n*m + m*m - m);
+z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+y = 2.4 * Math.Abs((x * x + b) / a) + (a-b)*Math.Pow(Math.Sin(a-b), 2) + Math.Pow(10, -2) * (x-b);
